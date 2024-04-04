ae107ae5c1f00c2fae895de072e875ba43cd054f
Lion_Game_Studio <144491199+LeoGod03@users.noreply.github.com>
2024-04-04 16:42:52 -0600

    Revert "cambios en alumnoDAO"
    This reverts commit f0da6b163ab0f04b4bd743da07c886a171e188de.

--- pruebaConexion/src/dao/AlumnoDao.java
+++ pruebaConexion/src/dao/AlumnoDao.java
@@ -7,6 +7,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.ResultSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import modelo.Alumno;
 /**
  *
